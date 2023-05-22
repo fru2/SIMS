22f31ef6a34fdd8f53cbe5f86f932e1b44bd2e42
Mohammad Arif Aziz <arifaziz2743@gmail.com>
2023-05-22 17:24:50 +0530

    Added Summary Activity

---
+++ app/src/main/java/com/patnacollege/sims/SummaryActivity.java
@@ -0,0 +1,18 @@
+package com.patnacollege.sims;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+
+    public void P1_bay(View view) {
+    }
+}

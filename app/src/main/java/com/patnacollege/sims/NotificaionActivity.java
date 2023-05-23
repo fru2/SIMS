0fe0e3231a7a37d21d6d117c790f3fddf6d4d3e2
fru2 <utsavfrutu@gmail.com>
2023-05-23 11:42:51 +0530

    New activities added

---
+++ app/src/main/java/com/patnacollege/sims/NotificaionActivity.java
@@ -0,0 +1,14 @@
+package com.patnacollege.sims;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificaionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notificaion);
+    }
+}

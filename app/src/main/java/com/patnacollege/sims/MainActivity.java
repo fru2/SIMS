e1e3570e68c5b89b16936c539b55d12844bf1a44
fru2 <utsavfrutu@gmail.com>
2023-05-25 16:28:36 +0530

    Firebase added.

--- app/src/main/java/com/patnacollege/sims/MainActivity.java
+++ app/src/main/java/com/patnacollege/sims/MainActivity.java
@@ -2,13 +2,37 @@ package com.patnacollege.sims;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FirebaseApp.initializeApp(this);
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        if (currentUser != null){
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
         setContentView(R.layout.activity_main);
+
+        Button getStartedBtn = (Button) findViewById(R.id.get_started_btn);
+        getStartedBtn.setOnClickListener(v -> {
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+        });
+
     }
 }

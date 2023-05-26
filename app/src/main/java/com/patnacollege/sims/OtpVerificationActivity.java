198923639686867f0a5571f32047782887c6836c
fru2 <utsavfrutu@gmail.com>
2023-05-26 06:55:46 +0530

    Bottom navigation bar added

--- app/src/main/java/com/patnacollege/sims/OtpVerificationActivity.java
+++ app/src/main/java/com/patnacollege/sims/OtpVerificationActivity.java
@@ -2,7 +2,9 @@ package com.patnacollege.sims;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class OtpVerificationActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class OtpVerificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_verification);
+
+        Button submitBtn = findViewById(R.id.submit_btn);
+        submitBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(OtpVerificationActivity.this, HomeNavigationActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
     }
 }
